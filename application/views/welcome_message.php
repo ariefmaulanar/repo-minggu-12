d6c9860d31ca2061f372c2e833da1f6ff5a732e8
ariefmaulanar <62791960+ariefmaulanar@users.noreply.github.com>
2020-06-11 20:38:23 +0700

    Add files via upload

---
+++ application/views/welcome_message.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Bootstrap Notification</title>
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap.min.css">
+	<script src="<?php echo base_url(); ?>assets/bootstrap.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/jquery-3.2.1.min.js"></script>
+</head>
+<style>
+	#notifications {
+		cursor: pointer;
+		position: fixed;
+		right: 0px;
+		z-index: 9999;
+		bottom: 0px;
+		margin-bottom: 22px;
+		margin-right: 15px;
+		min-width: 300px;
+		max-width: 800px;
+	}
+</style>
+
+<body>
+	<div id="notifications"><?php echo $this->session->flashdata('msg'); ?></div>
+	<br>
+	<div class="col-sm-6 col-sm-offset-4">
+		<form method="post" enctype="multipart/form-data" action="<?php echo base_url(); ?>index.php/welcome/insert_data" class="form-horizontal">
+			<div class="form-group">
+				<div class="col-sm-6">
+					<input type="text" class="form-control" name="kata" />
+				</div>
+				<div class="col-sm-6">
+					<button type="submit" class="btn btn-default ">Submit</button>
+				</div>
+			</div>
+		</form>
+	</div>
+
+	<script>
+		$('#notifications').slideDown('slow').delay(3000).slideUp('slow');
+	</script>
+</body>
+
+</html>
